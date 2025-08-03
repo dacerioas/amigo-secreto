0bb5eed194dee1e2ff1372eb4b7936fb7bfdc88f
Daniella Cedeno <dcedenoarias@gmail.com>
2025-08-03 14:35:37 -0600

    Actualiza el archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Primera parte del desafío
+let amigos =[];
+
+function limpiarCaja(){
+    document.getElementById('nombre').value = '';
+}
+
+function agregarAmigo(nombre) {
+    let nombreamigo = document.getElementById('nombre').value;
+    if (nombreamigo == ''){
+        alert(' Favor ingresar un nombre');
+
+    }else{
+        amigos.push(nombreamigo); 
+        limpiarCaja();
+    }
+}
+
