a677690bb566a6e9cfbdda82a729b516e07c5fdd
Daniella Cedeno <dcedenoarias@gmail.com>
2025-08-03 22:09:40 -0600

    Actualiza el archivo app.js

--- app.js
+++ app.js
@@ -11,12 +11,23 @@ function agregarAmigo() {
         alert(' Favor ingresar un nombre');
 
     }else{
+        //Agrego el elemento nuevo al array
         amigos.push(nombreamigo); 
+        // Determina la variable lista con el id del elemento HTML de la lista
         let lista = document.getElementById('listaAmigos');
+        //Seguro de limpieza de lista 
+        lista.innerHTML = '';
+        //Crea nuevo elemento de lista
+        for(let i =0; i<amigos.length ; i++){
+            //Crea un bucle para revisar cada elemento del array
         let nuevoAmigo = document.createElement('li');
-        nuevoAmigo.textContent = nombreamigo;
+        //Crea un nuevo elemento de lista
+        nuevoAmigo.textContent = amigos[i];
+        //Muestra el nombre del amigo en la lista
         lista.appendChild(nuevoAmigo);
+        console.log(amigos)
         limpiarCaja();
+        }
     }
 }
 
